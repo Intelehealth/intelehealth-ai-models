9ec9dd860bd80ce1d29e2ede314a336376604ade
Bharat Shetty B <bharat.shetty@gmail.com>
2024-11-25 18:19:39 +0000

    Add question in example input for trainset

--- utils/prepare_ddx_data.py
+++ utils/prepare_ddx_data.py
@@ -18,12 +18,16 @@ for index,row in new_df.iterrows():
 
     example = dspy.Example(
         case_id = case_id,
-        case_info = patient_case_prompt,
-        diagnosis = diagnosis
-    ).with_inputs("Patient Case Prompt")
+        question = "For the shared patient case output a top diagnosis string. " + patient_case_prompt,
+        answer = diagnosis
+    ).with_inputs("question")
 
     training_examples.append(example)
 
 print(len(training_examples))
 
 print(training_examples[1])
+
+
+def ret_training_examples():
+    return training_examples
