d79a83ec1fcf84c1541aa5c3e64e5bbc234fc8a2
Bharat Shetty B <bharat.shetty@gmail.com>
2024-11-27 08:29:31 +0000

    Data clean using regex and search

---
+++ utils/clean_ddx_data.py
@@ -0,0 +1,62 @@
+import dspy
+import pandas as pd
+
+df = pd.read_csv("../data/190_cases.csv")
+
+print(df.columns)
+
+new_df = df[["Case ID", "Diagnosis", "Patient Case Prompt"]]
+
+print(new_df)
+
+training_examples = []
+import sys
+for index,row in new_df.iterrows():
+    print("-------------------")
+    print(index)
+    case_id = row["Case ID"]
+    diagnosis = row["Diagnosis"]
+    patient_case_prompt = row["Patient Case Prompt"]
+    history = ""
+    prompt = ""
+    import re
+    
+    delimiter = """You are a doctor with the following patient rural India. Here is their case with the history of presenting illness, their physical exams, and demographics. What would be the top 5 differential diagnosis for this patient? Please rank the differential diagnoses based on the likelihood and provide a detailed explanation for each diagnosis. Please remember this is a patient in rural India and use this as a consideration for the diseases likely for the patient."""
+
+# Split the text
+    parts = patient_case_prompt.split(delimiter, maxsplit=1)
+    instruction = delimiter.strip()
+    case_details = parts[1].strip() if len(parts) > 1 else ""
+
+    # Output the results
+    # print("Instruction:\n", instruction)
+    # print("\nCase Details:\n", case_details)
+
+    if len(case_details) < 2:
+        parts = patient_case_prompt.split("HISTORY", maxsplit=1)
+        instruction = parts[0].strip()  # Everything before "HISTORY"
+        case_details = "HISTORY \n" + parts[1].strip() if len(parts) > 1 else ""  # Everything after "HISTORY"
+    
+    if len(case_details) < 2:
+        parts = patient_case_prompt.split("Hx", maxsplit=1)
+        instruction = parts[0].strip()  # Everything before "HISTORY"
+        case_details = "Hx \n" + parts[1].strip() if len(parts) > 1 else ""  # Everything after "HISTORY"
+
+    if len(case_details) < 2:
+        parts = patient_case_prompt.split(" Salient features", maxsplit=1)
+        instruction = parts[0].strip()  # Everything before "Salient Features"
+        case_details = " Salient features \n" + parts[1].strip() if len(parts) > 1 else ""  # Everything after "Salient Features"
+
+    training_examples.append({
+        "case_id": case_id,
+        "diagnosis": diagnosis,
+        "original": row["Patient Case Prompt"],
+        "patient_case_prompt": instruction,
+        "history": case_details
+    })
+    
+    
+
+training_df = pd.DataFrame(training_examples)
+print(training_df)
+training_df.to_csv("../data/seperated_patient_prompts.csv", index=False)
